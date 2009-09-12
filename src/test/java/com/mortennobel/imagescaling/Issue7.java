027acf2beda96e601c468c9a71032c419c409228
m@nobel-joergensen.com <m@nobel-joergensen.com@76e46de0-011b-11de-bc25-5f29055a2a2d>
2009-09-12 21:32:37 +0000

    Improve accurancy of resampling

--- src/test/java/com/mortennobel/imagescaling/Issue7.java
+++ src/test/java/com/mortennobel/imagescaling/Issue7.java
@@ -20,13 +20,14 @@ package com.mortennobel.imagescaling;
 import junit.framework.TestCase;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * The idea of this test, is to ensure that the subsampling method
  * works correct
  */
 public class Issue7 extends TestCase {
-	public void testSimpleCase() throws Exception {
+	public void testSimpleCaseDecrease() throws Exception {
 		int srcSize = 100;
 		int dstSize = 50;
 		ResampleOp.SubSamplingData subSamplingData = ResampleOp.createSubSampling(ResampleFilters.getBoxFilter(), srcSize,dstSize);
@@ -42,6 +43,25 @@ public class Issue7 extends TestCase {
 		}
 		for (int i=0;i<readFreq.length;i++){
 			assertEquals("All pixels should only be read exactly once "+Arrays.toString(readFreq),1,readFreq[i]);
+		}
+	}
+
+	public void testSimpleCaseIncrease() throws Exception {
+		int srcSize = 50;
+		int dstSize = 100;
+		ResampleOp.SubSamplingData subSamplingData = ResampleOp.createSubSampling(ResampleFilters.getBoxFilter(), srcSize,dstSize);
+		int[] readFreq = new int[srcSize];
+
+		for (int i=0;i<dstSize;i++){
+			int offset = i* subSamplingData.getNumContributors();
+			int max= subSamplingData.getArrN()[i];
+			for (int j=0;j<max;j++,offset++){
+				int valueLocation = subSamplingData.getArrPixel()[offset];
+				readFreq[valueLocation]++;
+			}
+		}
+		for (int i=0;i<readFreq.length;i++){
+			assertEquals("All pixels should only be read exactly twice "+Arrays.toString(readFreq),2,readFreq[i]);
 		}
 	}
 }
