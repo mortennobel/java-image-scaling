974c10690e5664ea1dfa4f8f8e6593b13dcfd277
m@nobel-joergensen.com <m@nobel-joergensen.com@76e46de0-011b-11de-bc25-5f29055a2a2d>
2009-11-02 21:20:53 +0000

    Add test-case for issue 10. Improved testcase 10. Also test for incompatible out images

--- src/test/java/com/mortennobel/imagescaling/Issue10.java
+++ src/test/java/com/mortennobel/imagescaling/Issue10.java
@@ -1,6 +1,7 @@
 package com.mortennobel.imagescaling;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -9,13 +10,37 @@ import java.awt.image.BufferedImage;
  * Problem when scaling 1 channel (Grayscale) image 
  */
 public class Issue10 extends TestCase {
+	/**
+	 * Test that the library can scale a grayscale image
+	 * @throws Exception should not occur
+	 */
 	public void testIssue10() throws Exception {
 		BufferedImage image2D = ImageIO.read(getClass().getResourceAsStream("test_issue10.jpg"));
 
-			DimensionConstrain dc = DimensionConstrain.createMaxDimensionNoOrientation(50,50);
+		DimensionConstrain dc = DimensionConstrain.createAbsolutionDimension(50,50);
 
+		ResampleOp  resampleOp = new ResampleOp (dc);
+		BufferedImage rescaledTomato = resampleOp.filter(image2D, null);
+
+	}
+
+	/**
+	 * Test that the library throws an exception if a destination image is not compatible with source img
+	 * @throws Exception should not occur
+	 */
+	public void testIssue10Exception() throws Exception {
+		RuntimeException re = null;
+		try{
+			BufferedImage image2D = ImageIO.read(getClass().getResourceAsStream("test_issue10.jpg"));
+
+			DimensionConstrain dc = DimensionConstrain.createAbsolutionDimension(50,50);
+			BufferedImage out = new BufferedImage(50,50, BufferedImage.TYPE_INT_ARGB);
 			ResampleOp  resampleOp = new ResampleOp (dc);
-			BufferedImage rescaledTomato = resampleOp.filter(image2D, null);
-
+			BufferedImage rescaledTomato = resampleOp.filter(image2D, out);
+		}
+		catch (RuntimeException e){
+			re = e;
+		}
+		assertNotNull("ISL should throw an exception since out image is not compatible", re);
 	}
 }
