03d75f00944c3efc023383d8b7d04e6b4d15f7aa
morten@nobel-joergensen.com <morten@nobel-joergensen.com@76e46de0-011b-11de-bc25-5f29055a2a2d>
2013-10-29 01:19:52 +0000

    Change license to BSD 3-Clause

--- src/test/java/com/mortennobel/imagescaling/Issue9.java
+++ src/test/java/com/mortennobel/imagescaling/Issue9.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2013, Morten Nobel-Joergensen
+ *
+ * License: The BSD 3-Clause License
+ * http://opensource.org/licenses/BSD-3-Clause
+ */
 package com.mortennobel.imagescaling;
 
 import junit.framework.TestCase;
